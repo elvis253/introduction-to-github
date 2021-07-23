114293bb1fae53789f0ccbd3436855be66d1076a
Elvis Araka <elvisarakaogesa@gmail.com>
2021-07-23 16:01:10 +0300

    Create customerlogin.php

---
+++ customerlogin.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>
+<?php session_start();
+<html>
+<head>
+	<title>login</title>
+	</head>
+	<body>
+		<form action="customerhome.php" method="post">
+			<fieldset>
+				<legend>Sign in</legend>
+				<table>
+					<tr>
+						<td>
+							<label for="email">Email</label>
+
+						</td>
+						<td>
+							<input type="email" id="email" name="email">
+				<br>
+
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label for="password">Password</label>
+
+						</td>
+						<td>
+							<input type="password" id="password" name="password">
+				<br>
+
+						</td>
+					</tr>
+				
+				<?php
+					if(isset($_SESSION['loginfai'])){
+						echo $_SESSION['loginfai'];
+						//echo  $SESSION['acountno'];
+	
+					}
+					?>
+					<?php if(isset($_SESSION['emai'])){
+						echo $_SESSION['emai'];
+						//echo  $SESSION['acountno'];
+	
+					}
+					?>
+					<?php if(isset($_SESSION['pass'])){
+						echo $_SESSION['pass'];
+						//echo  $SESSION['acountno'];
+	
+					}
+					?>
+					
+
+				<?php
+				session_destroy();
+				?>
+				
+				
+				
+			</table>
+			<input type="submit" name=submit>
+			</fieldset>
+		</form>
+	</body>
+
+</html>
